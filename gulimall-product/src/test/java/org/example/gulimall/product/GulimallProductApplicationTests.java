db4134d532a5ed0feeb75026bc868d8a70a3be9b
此昵称不存在 <878740953@qq.com>
2024-07-25 22:25:37 +0800

    谷粒商城项目-项目初始化

---
+++ gulimall-product/src/test/java/org/example/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
